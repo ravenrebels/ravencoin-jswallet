190e6c994241f8f0987dfba6799fc977b3ab0fd9
Dick Larsson <dicklarsson@gmail.com>
2023-05-14 10:15:00 +0200

    Add unit test for sweep

---
+++ src/sweep.test.js
@@ -0,0 +1,30 @@
+const RavencoinWallet = require("../dist/index.cjs");
+
+jest.setTimeout(20 * 1000);
+
+test("Test sweep", async () => {
+  /* 
+    The wallet that will be drained, that has the funds from start.
+
+    MNEMONIC: sight rate burger maid melody slogan attitude gas account sick awful hammer
+    MAINNET const WIF = "Kz5U4Bmhrng4o2ZgwBi5PjtorCeq2dyM7axGQfdxsBSwCKi5ZfTw";
+    TESTNET const WIF = "cUVdRNVobgjAw5jGWYkvbWmk42Vxzvte4btmsZ5qSqszdPi9M3Vy"
+  */
+  const WIF = "cUVdRNVobgjAw5jGWYkvbWmk42Vxzvte4btmsZ5qSqszdPi9M3Vy";
+  const network = "rvn-test";
+
+  //The wallet that will RECEIVE the funds
+  const wallet = await RavencoinWallet.createInstance({
+    mnemonic:
+      "frozen drift quiz glove wrong cycle glide increase hybrid arch endorse brisk",
+    network,
+  });
+
+  try {
+    const result = await wallet.sweep(WIF);
+    const nullOrString = typeof result.transactionId === "string" || null;
+    expect(true).toBe(nullOrString);
+  } catch (e) {
+    console.log("EXCEPTION", e);
+  }
+});
