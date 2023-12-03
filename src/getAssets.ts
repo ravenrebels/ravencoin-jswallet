b51980cf4c44e7c8afdcb030b61d2e1414277c5b
Dick Larsson <dicklarsson@gmail.com>
2023-12-03 12:24:12 +0100

    Add value property to getAssets output

--- src/getAssets.ts
+++ src/getAssets.ts
@@ -1,14 +1,20 @@
 import { methods } from "@ravenrebels/ravencoin-rpc";
 import { Wallet } from "./ravencoinWallet";
 
-export async function getAssets(wallet:Wallet, addresses: string[]) {
+export async function getAssets(wallet: Wallet, addresses: string[]) {
   const includeAssets = true;
   const params = [{ addresses: addresses }, includeAssets];
   const balance = (await wallet.rpc(methods.getaddressbalance, params)) as any;
 
   //Remove baseCurrency
+  //Convert from satoshis
   const result = balance.filter((obj) => {
-    return obj.assetName !== wallet.baseCurrency;
+    obj.assetName !== wallet.baseCurrency;
+    obj.value = 0;
+    if (obj.balance > 0) {
+      obj.value = obj.balance / 1e8;
+    }
+    return obj;
   });
   return result;
 }
